8ed36c888bdbc249f369707553ffdc81530fc749
MalenkiiMalchik <ajr166@georgetown.edu>
2023-06-15 16:44:49 -0400

    splyr

--- R/sc_fact_df.R
+++ R/sc_fact_df.R
@@ -54,7 +54,7 @@ sc_fact_df <- function(filepath = here::here("Data", "sc_fact"), outpath, downlo
            mot_soh = soh*mot) %>%
     dplyr::mutate(smp = lubridate::quarter(x = lubridate::ym(period), with_year = TRUE, fiscal_start = 10),
                   mer_pd = paste0("FY", substr(smp, 3, 4), "Q", substr(smp, 6, 6))) %>%
-    splyr::select(-smp)
+    dplyr::select(-smp)
 
   # Save processed file
   readr::write_csv(sc_fact, paste0(filepath, "sc_fact_processed_", Sys.Date(), ".csv"))
