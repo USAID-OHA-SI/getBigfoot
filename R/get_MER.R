33a99e7777f24046690df1fcaa7a2e3f0846b3d7
MalenkiiMalchik <ajr166@georgetown.edu>
2023-07-17 12:07:57 -0400

    Added a mer download option

--- R/get_MER.R
+++ R/get_MER.R
@@ -1,13 +1,29 @@
 #' get_MER
 #'
-#' @param path The path to the raw MSD file
+#' @param path The path to the raw MSD file or the folder to save it in if download == T
+#' @param download Whether to download a new MSD file, defaults to FALSE
 #' @importFrom magrittr %>%
 #' @export
 
 
-get_mer <- function(path) {
+get_mer <- function(path = here::here("Data"), download = F) {
+
+  if(download == T){
+    # Download zip file and arrange by date
+    folder <- "1d5dhTK_UCY2RaEW_m-JHKUWjBEkWggNs"
+    files_in_folder <- googledrive::drive_ls(googledrive::as_id(folder)) %>%
+      googledrive::drive_reveal("created_time") %>%
+      dplyr::arrange(created_time)
+    googledrive::drive_download(file = files_in_folder$id[1],
+                                path = paste0(path, "/", files_in_folder$name[1]))
+
+    # Unzip and remove zip file
+    unzip(paste0(path, "/", files_in_folder$name[1]), exdir = path)
+    file.remove(paste0(path, "/", files_in_folder$name[1]))
+  }
 
   df = gophr::read_psd(path)
+
 
   msd = df %>%
     dplyr::filter(
