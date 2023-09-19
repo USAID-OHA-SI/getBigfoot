1a0d551160d76c6f30377c6802bc29337a1a117a
MalenkiiMalchik <ajr166@georgetown.edu>
2023-09-19 11:59:08 -0400

    Added a method for reading in local sc_fact

--- R/get_scfact.R
+++ R/get_scfact.R
@@ -2,12 +2,14 @@
 #'
 #' @param datapath The path for downloaded data
 #' @param upload Whether to upload sc_fact_collated, defaults to F
+#' @param download Whether to download a new file or look for a local one, defaults to T
 #'
 #' @importFrom magrittr %>%
 #' @export
 
-get_scfact <- function(datapath = here::here("Data"), upload = F){
+get_scfact <- function(datapath = here::here("Data"), upload = F, download = T){
 
+  if(download == T){
   # Download zip file and arrange by date
   folder <- "1nRslZsUFvjdTN1Kp6pzFk7h3ZWJE-xXX"
   files_in_folder <-
@@ -38,12 +40,25 @@ get_scfact <- function(datapath = here::here("Data"), upload = F){
     file.remove(paste0(datapath, "/sc_fact/", file))
   }
   readr::write_csv(sc_fact, paste0(datapath, "/sc_fact/", "sc_fact_collated_", Sys.Date(), ".csv"))
-
+}
 
   if(upload == T){
   # Write collated file to drive
   googledrive::drive_put(media = paste0(datapath, "/sc_fact/", "sc_fact_collated_", Sys.Date(), ".csv"), path = googledrive::as_id("1A2VfmKOxfmmN3h3P_hTI-B5V1B3KSZlx"),
                          name = paste0("sc_fact_collated_", Sys.Date(), ".csv"))
+  }
+  if(download == F){
+    if(length(list.files(paste0(datapath, "/sc_fact"), patt = "sc_fact_collated*"))>0){
+      sc_fact = list.files(paste0(datapath, "/sc_fact"), patt = "sc_fact_collated*") %>%
+        tibble::as_tibble() %>%
+        dplyr::mutate(date = stringr::str_remove(value, "sc_fact_collated_")) %>%
+        dplyr::mutate(date = lubridate::as_date(stringr::str_remove(date, ".csv"))) %>%
+        dplyr::arrange(desc(date))
+      sc_fact = readr::read_csv(paste0(datapath, "/sc_fact/", sc_fact$value[1]))
+    } else {
+      print("No local version of sc_fact found on datapath.")
+      sc_fact = NULL
+    }
   }
 
   return(sc_fact)
